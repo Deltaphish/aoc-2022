39e1adfb14672b19fdb6105864f6be88a9424ce9
Joakim <joakimanderlind@gmail.com>
2022-12-04 23:23:03 +0100

    day 04 with perl

---
+++ perl/day04/p2.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use v5.36;
+
+open(my $in, "<", "input") or die "Cant open file $!";
+
+my $count = 0;
+while (<$in>) {
+    if(/(\d+)-(\d+),(\d+)-(\d+)/){
+        if($1 <= $4 && $2 >= $4){
+            $count += 1;
+        }
+        elsif($1 <= $3 && $2 >= $3){
+            $count += 1;
+        }
+        elsif($1 >= $3 && $2 <= $4){
+        $count += 1;
+        }
+        elsif($3 >= $1 && $4 <= $2){
+        $count += 1;
+        }
+    }
+}
+print $count;
